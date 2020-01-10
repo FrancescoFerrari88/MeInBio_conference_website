2225f4b69d4b665a1b55c620e639cce6034bd2ba
Francesco Ferrari <francesco.ferrari.1988@gmail.com>
2020-01-10 23:01:40 +0100

    password_forgot

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,6 +8,12 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('login/', views.CustomLoginView.as_view(), name="login"),
     path('logout/', views.CustomLogoutView.as_view(), name="logout"),
+    path('password-reset/',
+         auth_views.PasswordResetView.as_view(template_name='password_reset.html'),
+         name="password_reset"),
+    path('password-reset/done/',
+         auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'),
+         name="passowrd_reset_done"),
     path('submit/', createContributorView.as_view(), name='submit'),
     path('update/<int:pk>/', updateContributorView.as_view(), name='update'),
     path('delete/<int:pk>/', deleteContributorView.as_view(), name='delete')
