2eec1874833f97023225b7a97cf8c90e6ef2b64b
Francesco Ferrari <francesco.ferrari.1988@gmail.com>
2020-01-13 14:23:05 +0100

    environ variables

--- MeInBio_conference/settings.py
+++ MeInBio_conference/settings.py
@@ -20,12 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'h=@)0!exe0jgj8si8ua62305%hjh7a2hzwfyjmt8gl5o_^g*cv'
+#SECRET_KEY = 'h=@)0!exe0jgj8si8ua62305%hjh7a2hzwfyjmt8gl5o_^g*cv'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG_VALUE')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["meinbio-symposium.herokuapp.com"]
 
 
 # Application definition
